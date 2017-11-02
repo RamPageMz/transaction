19bf215ba38f611b51d18ee9f6f89579732151a9
RamPageMz <1033516561@qq.com>
2017-11-02 13:29:31 +0800

    test merge laptop to master

--- src/code/Main.java
+++ src/code/Main.java
@@ -18,8 +18,14 @@ public class Main {
         //定义事务的操作信息  每个事务的操作数个数随机
         Transaction[] transactionArray=new Transaction[transactionSize+1];
         for (int i=1;i<=transactionSize;i++){
+            //事务生成 创建名称 T1 T2...
             transactionArray[i]=new Transaction("T"+i);
 
+            //生成事务操作数
+            for(int j=1;j<=new Random().nextInt(transactionOperationSize);j++){
+
+
+            }
 
         }
 
